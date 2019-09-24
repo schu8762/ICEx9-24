af49a58d45002e7cf8c094f98bef9d6fd09da117
schu8762 <J.Schuller@ou.edu>
2019-09-24 11:18:47 -0500

    first collection

--- ICEx9-24/Program.cs
+++ ICEx9-24/Program.cs
@@ -10,6 +10,34 @@ namespace ICEx9_24
     {
         static void Main(string[] args)
         {
+            int[] studentIDs = new int[3];
+            studentIDs[0] = 1;
+            studentIDs[1] = 2;
+            studentIDs[2] = 3;
+            double[] gpas = { 2.5, 3.1, 4.0 }; //new double [3]
+
+
+            Console.WriteLine("Please pick an ID from the list to see their gpa: ");
+
+            foreach (int id in studentIDs)
+            {
+                Console.Write(" " + id);
+            }
+
+            Console.WriteLine();
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < studentIDs.Length; i++)
+            {
+                if(studentIDs[i] == userInput)
+                {
+                    Console.WriteLine($"The GPA for the student with id {userInput} is {gpas[i]}");
+                    break;
+                }
+
+            }
+
+            Console.ReadKey();
         }
     }
 }
